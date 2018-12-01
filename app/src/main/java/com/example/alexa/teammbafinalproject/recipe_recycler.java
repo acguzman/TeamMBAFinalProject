7b4aaed0ad6f6ca96fdd96f7601ed25d6eeb4c3f
Patrick Hunt <prhunt@umich.edu>
2018-11-30 20:30:41 -0500

    Added in a recycler view based layout system for the recipie description activity.

---
+++ app/src/main/java/com/example/alexa/teammbafinalproject/recipe_recycler.java
@@ -0,0 +1,13 @@
+package com.example.alexa.teammbafinalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class recipe_recycler extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_recycler);
+    }
+}
