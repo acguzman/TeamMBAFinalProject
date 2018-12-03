94b86be5e78ef517d464cff7ed758d0a79ea48c3
jadnson <jadnson@umich.edu>
2018-12-03 10:54:20 -0500

    Re-commiting step activities and some drawable files for icons

---
+++ app/src/main/java/com/example/alexa/teammbafinalproject/IngredientBase.java
@@ -0,0 +1,9 @@
+package com.example.alexa.teammbafinalproject;
+
+public class IngredientBase {
+    public String measuredingredient;
+
+    IngredientBase(String measurement, String ingredient) {
+        this.measuredingredient = measuredingredient;
+    }
+}
