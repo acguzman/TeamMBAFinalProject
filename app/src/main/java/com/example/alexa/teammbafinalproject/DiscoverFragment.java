3a2ac52dacda6fed35c8be3648223c0abf46ac3b
anuj4 <anujagr@umich.edu>
2018-12-07 15:35:25 -0500

    new fragments for bottom navigation bar

--- app/src/main/java/com/example/alexa/teammbafinalproject/DiscoverFragment.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/DiscoverFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment1 extends Fragment {
+public class DiscoverFragment extends Fragment {
 
 
-    public HomeFragment1() {
+    public DiscoverFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class HomeFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_fragment1, container, false);
+        return inflater.inflate(R.layout.fragment_discover, container, false);
     }
 
 }
