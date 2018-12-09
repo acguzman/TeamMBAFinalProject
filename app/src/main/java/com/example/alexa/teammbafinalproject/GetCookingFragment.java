84109e03fbfa0b07d1e7acca6fc01f6c2b7e8123
jadnson <jadnson@umich.edu>
2018-12-09 05:49:33 -0500

    Recipe Step Complete visible now Added code to Get Cooking fragment to enable button to RecipeStepBase --- emulator keeps crashing so haven't been able to test

--- app/src/main/java/com/example/alexa/teammbafinalproject/GetCookingFragment.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/GetCookingFragment.java
@@ -1,18 +1,21 @@
 package com.example.alexa.teammbafinalproject;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GetCookingFragment extends Fragment {
+public class GetCookingFragment extends Fragment implements View.OnClickListener {
 
+    Button buttonGetCookingAvocadoFettucine, buttonGetCookingMushroomFrittata, buttonGetCookingKoreanSpicyChx;
 
     public GetCookingFragment() {
         // Required empty public constructor
@@ -22,8 +25,32 @@ public class GetCookingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        buttonGetCookingAvocadoFettucine = getView().findViewById(R.id.buttonGetCookingAvocadoFettucine);
+        buttonGetCookingKoreanSpicyChx = getView().findViewById(R.id.buttonGetCookingKoreanSpicyChx);
+        buttonGetCookingMushroomFrittata = getView().findViewById(R.id.buttonGetCookingMushroomFrittata);
+
+        buttonGetCookingAvocadoFettucine.setOnClickListener(this);
+        buttonGetCookingKoreanSpicyChx.setOnClickListener(this);
+        buttonGetCookingMushroomFrittata.setOnClickListener(this);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_get_cooking, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        if(v == buttonGetCookingAvocadoFettucine){
+
+            Intent intentRecipeStepBase = new Intent(getActivity(),RecipeStepBase.class);
+            startActivity(intentRecipeStepBase);
+
+        }else if(v == buttonGetCookingKoreanSpicyChx){
+
+        }else if(v == buttonGetCookingMushroomFrittata){
+
+        }
+
+    }
 }
