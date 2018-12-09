cd251b517498f71fda5f659a560821d5f4001224
jadnson <jadnson@umich.edu>
2018-12-09 05:00:03 -0500

    Added code to recipe step base updated layout on add recipes layout updated recipe step base layout for scrolling Updated button names on get cooking cant figure out how to link to recipe step base from fragment and pull in which button was clicked to get there (so we can populate the right data)

--- app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepBase.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepBase.java
@@ -1,13 +1,51 @@
 package com.example.alexa.teammbafinalproject;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RecipeStepBase extends Activity {
+public class RecipeStepBase extends Activity implements View.OnClickListener {
+
+    Button floatingActionButtonNext, floatingActionButtonChat;
+    TextView textViewRecipeName, textViewStepName,textViewStepIngredientList;
+    EditText editTextRecipeName;
+    Drawable imageStepPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_step_base);
+
+        floatingActionButtonChat = (Button) findViewById(R.id.floatingActionButtonChat);
+        floatingActionButtonNext = (Button) findViewById(R.id.floatingActionButtonNext);
+        textViewRecipeName = (TextView) findViewById(R.id.textViewRecipeName);
+        textViewStepName = (TextView) findViewById(R.id.textViewStepName);
+        textViewStepIngredientList = (TextView) findViewById(R.id.textViewStepIngredientList);
+        editTextRecipeName = (EditText) findViewById(R.id.editTextRecipeName);
+
+        floatingActionButtonNext.setOnClickListener(this);
+        floatingActionButtonChat.setOnClickListener(this);
+
+        autoPopulate();
+
+        Toast.makeText(this, editTextRecipeName.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    private void autoPopulate() {
+        //       textViewStepName.setText()
+    }
+
+    //   @Override
+    public void onClick(View v) {
+        if(v == floatingActionButtonChat){
+
+        }else if(v == floatingActionButtonNext){
+
+        }
     }
 }
