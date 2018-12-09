562e62ddda1e7e6479cd8fa24d891fca6e88b6f6
aayush2345 <aayushagarwal21@gmail.com>
2018-12-09 14:40:58 -0500

    Updated GetCookingFragment to initialize view before it can be used.

--- app/src/main/java/com/example/alexa/teammbafinalproject/GetCookingFragment.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/GetCookingFragment.java
@@ -15,7 +15,8 @@ import android.widget.Button;
  */
 public class GetCookingFragment extends Fragment implements View.OnClickListener {
 
-    Button buttonGetCookingAvocadoFettucine, buttonGetCookingMushroomFrittata, buttonGetCookingKoreanSpicyChx;
+    Button buttonGetCookingAvocadoFettucine, buttonGetCookingMushroomFrittata,
+            buttonGetCookingKoreanSpicyChx;
 
     public GetCookingFragment() {
         // Required empty public constructor
@@ -26,29 +27,33 @@ public class GetCookingFragment extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        buttonGetCookingAvocadoFettucine = getView().findViewById(R.id.buttonGetCookingAvocadoFettucine);
-        buttonGetCookingKoreanSpicyChx = getView().findViewById(R.id.buttonGetCookingKoreanSpicyChx);
-        buttonGetCookingMushroomFrittata = getView().findViewById(R.id.buttonGetCookingMushroomFrittata);
+        View currView = inflater.inflate(R.layout.fragment_get_cooking, container, false);
+
+        buttonGetCookingAvocadoFettucine = currView.findViewById(R.id
+                .buttonGetCookingAvocadoFettucine);
+        buttonGetCookingKoreanSpicyChx = currView.findViewById(R.id.buttonGetCookingKoreanSpicyChx);
+        buttonGetCookingMushroomFrittata = currView.findViewById(R.id
+                .buttonGetCookingMushroomFrittata);
 
         buttonGetCookingAvocadoFettucine.setOnClickListener(this);
         buttonGetCookingKoreanSpicyChx.setOnClickListener(this);
         buttonGetCookingMushroomFrittata.setOnClickListener(this);
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_cooking, container, false);
+        return currView;
     }
 
     @Override
     public void onClick(View v) {
 
-        if(v == buttonGetCookingAvocadoFettucine){
+        if (v == buttonGetCookingAvocadoFettucine) {
 
-            Intent intentRecipeStepBase = new Intent(getActivity(),RecipeStepBase.class);
+            Intent intentRecipeStepBase = new Intent(getActivity(), RecipeStepBase.class);
             startActivity(intentRecipeStepBase);
 
-        }else if(v == buttonGetCookingKoreanSpicyChx){
+        } else if (v == buttonGetCookingKoreanSpicyChx) {
 
-        }else if(v == buttonGetCookingMushroomFrittata){
+        } else if (v == buttonGetCookingMushroomFrittata) {
 
         }
 
