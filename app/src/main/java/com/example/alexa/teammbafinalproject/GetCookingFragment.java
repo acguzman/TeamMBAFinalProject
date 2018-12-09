96ab6ff983de6cccdb9770b41b596007f947672f
jadnson <jadnson@umich.edu>
2018-12-09 01:14:18 -0500

    Added navigation to RecipeStepBase and created new activity for recipe step complete. Removed recycler versions of step pages

--- app/src/main/java/com/example/alexa/teammbafinalproject/GetCookingFragment.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/GetCookingFragment.java
@@ -1,29 +1,57 @@
 package com.example.alexa.teammbafinalproject;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GetCookingFragment extends Fragment {
+public class GetCookingFragment extends Fragment implements View.OnClickListener{
 
+    public Button buttonGetCookingAvocadoFettucine;
+    public Button buttonGetCookingKoreanSpicyChx;
+    public Button buttonGetCookingMushroomFrittata;
 
     public GetCookingFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        buttonGetCookingAvocadoFettucine = getView().findViewById(R.id.buttonGetCookingAvocadoFettucine);
+
+        buttonGetCookingAvocadoFettucine.setOnClickListener(this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_get_cooking, container, false);
-    }
 
+    }
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == buttonGetCookingAvocadoFettucine) {
+            Intent intentRecipeStepBase = new Intent(getActivity().getApplication(),RecipeStepBase.class);
+            startActivity(intentRecipeStepBase);
+
+        }
+    }
 }
