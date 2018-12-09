807ec4a52308d70028955b6d4bb53ad9501c25ab
aayush2345 <aayushagarwal21@gmail.com>
2018-12-09 16:11:13 -0500

    Changed button variables to FloatingActionButtons and altered a test toast.

--- app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepBase.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepBase.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +13,9 @@ import android.widget.Toast;
 
 public class RecipeStepBase extends Activity implements View.OnClickListener {
 
-    Button floatingActionButtonNext,floatingActionButtonNext2,floatingActionButtonNext3, floatingActionButtonChat;
-    TextView textViewRecipeName, textViewStepName,textViewStepIngredientList;
+    FloatingActionButton floatingActionButtonNext, floatingActionButtonNext2,
+            floatingActionButtonNext3, floatingActionButtonChat;
+    TextView textViewRecipeName, textViewStepName, textViewStepIngredientList;
     EditText editTextRecipeName;
     Drawable imageStepPhoto;
 
@@ -22,10 +24,10 @@ public class RecipeStepBase extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_step_base);
 
-        floatingActionButtonChat = (Button) findViewById(R.id.floatingActionButtonChat);
-        floatingActionButtonNext = (Button) findViewById(R.id.floatingActionButtonNext);
-        floatingActionButtonNext2 = (Button) findViewById(R.id.floatingActionButtonNext2);
-        floatingActionButtonNext3 = (Button) findViewById(R.id.floatingActionButtonNext3);
+        floatingActionButtonChat = findViewById(R.id.floatingActionButtonChat);
+        floatingActionButtonNext = findViewById(R.id.floatingActionButtonNext);
+        floatingActionButtonNext2 = findViewById(R.id.floatingActionButtonNext2);
+        floatingActionButtonNext3 = findViewById(R.id.floatingActionButtonNext3);
         textViewRecipeName = (TextView) findViewById(R.id.textViewRecipeName);
         textViewStepName = (TextView) findViewById(R.id.textViewStepName);
         textViewStepIngredientList = (TextView) findViewById(R.id.textViewStepIngredientList);
@@ -36,45 +38,53 @@ public class RecipeStepBase extends Activity implements View.OnClickListener {
 
         autoPopulate();
 
-        Toast.makeText(this, editTextRecipeName.getText().toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, textViewRecipeName.getText().toString(), Toast.LENGTH_LONG).show();
     }
 
     private void autoPopulate() {
         textViewRecipeName.setText("Avocado Fettuccine");
-        textViewStepName.setText("Step 1: Combine all of the ingredients, except fettucine, in a food processor. Process until smooth and creamy.");
-        textViewStepIngredientList.setText("2 whole Avocados, pitted" + "\n" + "1 Tablespoon Nutritional yeast" + "\n" +
-                "1 Tablespoon Extra virgin olive oil" + "\n" + "1 Tablespoon Fresh lemon juice" + "\n" +
-                "1 Clove Garlic, minced" + "\n" + "1 Teaspoon Salt" + "\n" + "¼ Teaspoon Freshly ground black pepper"
+        textViewStepName.setText("Step 1: Combine all of the ingredients, except fettucine, in a " +
+                "food processor. Process until smooth and creamy.");
+        textViewStepIngredientList.setText("2 whole Avocados, pitted" + "\n" + "1 Tablespoon " +
+                "Nutritional yeast" + "\n" +
+                "1 Tablespoon Extra virgin olive oil" + "\n" + "1 Tablespoon Fresh lemon juice" +
+                "\n" +
+                "1 Clove Garlic, minced" + "\n" + "1 Teaspoon Salt" + "\n" + "¼ Teaspoon Freshly " +
+                "ground black pepper"
         );
 
     }
 
     //   @Override
     public void onClick(View v) {
-        if(v == floatingActionButtonChat){
+        if (v == floatingActionButtonChat) {
 
             //not ready for this yet
 
-        }else if(v == floatingActionButtonNext){
+        } else if (v == floatingActionButtonNext) {
             textViewRecipeName.setText("Avocado Fettuccine");
-            textViewStepName.setText("Step 2: Prepare pasta according to package directions, in a saucepan. Drain; Add avocado mixture to pasta pan.");
+            textViewStepName.setText("Step 2: Prepare pasta according to package directions, in a" +
+                    " saucepan. Drain; Add avocado mixture to pasta pan.");
             textViewStepIngredientList.setText("3/4 Pound Fettuccine");
 
             floatingActionButtonNext2.setVisibility(View.VISIBLE);
             floatingActionButtonNext.setVisibility(View.INVISIBLE);
 
-        }else if (v == floatingActionButtonNext2){
+        } else if (v == floatingActionButtonNext2) {
 
             textViewRecipeName.setText("Avocado Fettuccine");
-            textViewStepName.setText("Step 3: Prepare pasta according to package directions, in a saucepan. Drain; Add avocado mixture to pasta pan.");
-            textViewStepIngredientList.setText("Toss pasta gently until sauce is evenly incorporated into the fettuccine.");
+            textViewStepName.setText("Step 3: Prepare pasta according to package directions, in a" +
+                    " saucepan. Drain; Add avocado mixture to pasta pan.");
+            textViewStepIngredientList.setText("Toss pasta gently until sauce is evenly " +
+                    "incorporated into the fettuccine.");
 
             floatingActionButtonNext3.setVisibility(View.VISIBLE);
             floatingActionButtonNext2.setVisibility(View.INVISIBLE);
 
-        }else if (v == floatingActionButtonNext3) {
+        } else if (v == floatingActionButtonNext3) {
 
-            Intent intentRecipeStepComplete = new Intent(this,RecipeStepComplete.class);
+            Intent intentRecipeStepComplete = new Intent(this, RecipeStepComplete.class);
             startActivity(intentRecipeStepComplete);
         }
     }
