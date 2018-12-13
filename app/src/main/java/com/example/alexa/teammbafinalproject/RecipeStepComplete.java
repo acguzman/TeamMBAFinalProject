81ee41ea4b274cce2f19534f75de92b4d75b4867
jadnson <jadnson@umich.edu>
2018-12-13 18:51:30 -0500

    RecipeStepComplete now successfully submits review data into review class on firebase

--- app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepComplete.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepComplete.java
@@ -9,6 +9,13 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class RecipeStepComplete extends Activity implements RatingBar.OnRatingBarChangeListener {
 
@@ -17,12 +24,16 @@ public class RecipeStepComplete extends Activity implements RatingBar.OnRatingBa
     ImageButton imageButtonUploadPhoto;
     Button buttonSubmitReview;
     RatingBar ratingBarEntry;
+    FirebaseDatabase fdb;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_step_complete);
+
+        mAuth = FirebaseAuth.getInstance();
 
         textViewRecipeName = (TextView) findViewById(R.id.textViewRecipeName);
         editTextCommentEntry = (EditText) findViewById(R.id.editTextCommentEntry);
@@ -32,31 +43,43 @@ public class RecipeStepComplete extends Activity implements RatingBar.OnRatingBa
 
         ratingBarEntry.setOnRatingBarChangeListener((RatingBar.OnRatingBarChangeListener) this);
 
-        imageButtonUploadPhoto.setOnClickListener(new View.OnClickListener() {
+        imageButtonUploadPhoto.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 imageButtonUploadPhotoClicked();
+            }
+        });
+        buttonSubmitReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 buttonSubmitReviewClicked();
+
             }
         });
 
+    }
+
+
+    public void buttonSubmitReviewClicked() {
+     //   put here code to upload review information into review class
+
+        fdb = FirebaseDatabase.getInstance();
+        DatabaseReference myReview = fdb.getReference("Review");
+
+        Review newReview = new Review(UUID.randomUUID().toString(), textViewRecipeName.getText().toString(),
+                0, editTextCommentEntry.getText().toString(),mAuth.getCurrentUser().getEmail());
+
+        myReview.push().setValue(newReview);
+    }
+
+    public void imageButtonUploadPhotoClicked() {
+        //put here how to upload photo from phone
     }
 
     @Override
     public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
         //put here what happens when rating is clicked
-    }
-
-
-    public void imageButtonUploadPhotoClicked() {
-        //put here code for opening camera and uploading photo
-
-    }
-
-    public void buttonSubmitReviewClicked() {
-        //put here code to upload review information into review class
-
     }
 
 
--- app/src/main/java/com/example/alexa/teammbafinalproject/Review.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/Review.java
@@ -4,18 +4,24 @@ import java.util.Date;
 
 public class Review {
 
-    String recipeID;
+    String reviewID;
+    String recipeName;
     int stars;
     String comment;
     String userID;
     //Date reviewDate; removing this for now, dates can get complicated - Pat
     //Picture?
 
-    public Review(String recipeID, int stars, String comment, String userID) {
-        this.recipeID = recipeID;
+    public Review(String reviewID, String recipeName, int stars, String comment, String userID) {
+        this.reviewID = reviewID;
+        this.recipeName = recipeName;
         this.stars = stars;
         this.comment = comment;
         this.userID = userID;
         //this.reviewDate = reviewDate; Removing this for now-Pat
+    }
+
+    public Review(){
+
     }
 }
