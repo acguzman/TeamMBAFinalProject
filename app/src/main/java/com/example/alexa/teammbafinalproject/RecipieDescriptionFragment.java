0ae9e335dfa1e3b4add6e8f07f7e082547f6405b
Patrick Hunt <prhunt@umich.edu>
2018-12-13 23:39:56 -0500

    Reviews now pull from Firebase.

--- app/src/main/java/com/example/alexa/teammbafinalproject/RecipieDescriptionFragment.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/RecipieDescriptionFragment.java
@@ -4,12 +4,20 @@ package com.example.alexa.teammbafinalproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -24,28 +32,49 @@ public class RecipieDescriptionFragment extends Fragment {
         // Required empty public constructor
     }
     private ArrayList<Review> reviews;
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         inflateReviewRecycler = inflater.inflate(R.layout.fragment_recipie_description, container, false);
-        initContacts();
+        reviews = new ArrayList<>();
+        initRecyclerView();
+        getContacts();
         return inflateReviewRecycler;
     }
-    private void initContacts() { //this is a fake reviews list to test the recycler view
-        reviews = new ArrayList<>();
-        reviews.add(new Review("555","Chicken", 3, "This recipie is amazing!", "ABourdain"));
-        reviews.add(new Review("556","pork", 4, "This recipie is the mayor of flavortown!", "GFierri"));
-        initRecyclerView();
+    private void getContacts() {  //use contactsRef.limitToLast(10).addChildEventListnener....
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference contactsRef = database.getReference("Review");
+        // Read from the database
+        contactsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for(DataSnapshot child : dataSnapshot.getChildren()) {
+                    Review contact = child.getValue(Review.class);
+                    reviews.add(contact);
+                 }
+                 recyclerViewAdapter.notifyDataSetChanged();
+                }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
     }
     private void initRecyclerView() {
         RecyclerView recyclerView = inflateReviewRecycler.findViewById(R.id.recycler_view_description);
-        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(reviews);
+        recyclerViewAdapter = new RecyclerViewAdapter(reviews);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 }
 
 /**
- *
+ *test reviews
+ reviews.add(new Review("555","Chicken", 3, "This recipie is amazing!", "ABourdain"));
+ reviews.add(new Review("556","pork", 4, "This recipie is the mayor of flavortown!", "GFierri"));
 
  **/
