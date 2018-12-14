98e97e8be3b4ad2c91cb2704b7edc4c4da61bf3c
Patrick Hunt <prhunt@umich.edu>
2018-12-13 19:51:42 -0500

    Fixed recycler view for reviews in description page.

--- app/src/main/java/com/example/alexa/teammbafinalproject/RecipieDescriptionFragment.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/RecipieDescriptionFragment.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class RecipieDescriptionFragment extends Fragment {
+    View inflateReviewRecycler;
 
     public RecipieDescriptionFragment() {
         // Required empty public constructor
@@ -26,24 +27,25 @@ public class RecipieDescriptionFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_recipie_description, container, false);
+        inflateReviewRecycler = inflater.inflate(R.layout.fragment_recipie_description, container, false);
+        initContacts();
+        return inflateReviewRecycler;
     }
-
+    private void initContacts() { //this is a fake reviews list to test the recycler view
+        reviews = new ArrayList<>();
+        reviews.add(new Review("555","Chicken", 3, "This recipie is amazing!", "ABourdain"));
+        reviews.add(new Review("556","pork", 4, "This recipie is the mayor of flavortown!", "GFierri"));
+        initRecyclerView();
+    }
+    private void initRecyclerView() {
+        RecyclerView recyclerView = inflateReviewRecycler.findViewById(R.id.recycler_view_description);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(reviews);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
 }
 
 /**
- * initContacts();
- private void initContacts() { //this is a fake reviews list to test the recycler view
- reviews = new ArrayList<>();
- reviews.add(new Review("555",3, "This recipie is amazing!", "ABourdain"));
- reviews.add(new Review("556",4, "This recipie is the president of flavortown!", "GFierri"));
- initRecyclerView();
- }
- private void initRecyclerView() {
- RecyclerView recyclerView = getView().findViewById(R.id.recycler_view);
- RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(reviews, this.getContext());
- recyclerView.setAdapter(recyclerViewAdapter);
- recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
- }
+ *
+
  **/
