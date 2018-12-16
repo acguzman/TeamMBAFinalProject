64495dd7e71e696c4a8c95da68350b5c05aa0455
jadnson <jadnson@umich.edu>
2018-12-15 22:11:32 -0500

    Recipe step base and recipe step complete are now finished, tested, and working properly!

--- app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepBase.java
+++ app/src/main/java/com/example/alexa/teammbafinalproject/RecipeStepBase.java
@@ -26,7 +26,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class RecipeStepBase extends AppCompatActivity implements View.OnClickListener {
 
     FloatingActionButton floatingActionButtonNext, floatingActionButtonNext2,
-            floatingActionButtonNext3, floatingActionButtonChat;
+            floatingActionButtonNext3, floatingActionButtonNext4,
+            floatingActionButtonNext5,floatingActionButtonNext6,
+            floatingActionButtonNext7,floatingActionButtonChat;
     TextView textViewRecipeName, textViewStepName, textViewStepIngredientList;
     ImageView imageStepPhoto;
     MenuItem home_menu_item, admin_add_recipe_menu_item, logout_menu_item;
@@ -40,6 +42,10 @@ public class RecipeStepBase extends AppCompatActivity implements View.OnClickLis
         floatingActionButtonNext = findViewById(R.id.floatingActionButtonNext);
         floatingActionButtonNext2 = findViewById(R.id.floatingActionButtonNext2);
         floatingActionButtonNext3 = findViewById(R.id.floatingActionButtonNext3);
+        floatingActionButtonNext4 = findViewById(R.id.floatingActionButtonNext4);
+        floatingActionButtonNext5 = findViewById(R.id.floatingActionButtonNext5);
+        floatingActionButtonNext6 = findViewById(R.id.floatingActionButtonNext6);
+        floatingActionButtonNext7 = findViewById(R.id.floatingActionButtonNext7);
         textViewRecipeName = (TextView) findViewById(R.id.textViewRecipeName);
         textViewStepName = (TextView) findViewById(R.id.textViewCongrats);
         textViewStepIngredientList = (TextView) findViewById(R.id.textViewStepIngredientList);
@@ -52,6 +58,10 @@ public class RecipeStepBase extends AppCompatActivity implements View.OnClickLis
         floatingActionButtonNext.setOnClickListener(this);
         floatingActionButtonNext2.setOnClickListener(this);
         floatingActionButtonNext3.setOnClickListener(this);
+        floatingActionButtonNext4.setOnClickListener(this);
+        floatingActionButtonNext5.setOnClickListener(this);
+        floatingActionButtonNext6.setOnClickListener(this);
+        floatingActionButtonNext7.setOnClickListener(this);
         floatingActionButtonChat.setOnClickListener(this);
 
         autoPopulate();
@@ -101,18 +111,25 @@ public class RecipeStepBase extends AppCompatActivity implements View.OnClickLis
         } else if (v == floatingActionButtonNext) {
 
             String findName = textViewRecipeName.getText().toString();
-
             myRef.orderByChild("recipeName").equalTo(findName).addChildEventListener(new ChildEventListener() {
-
 
                 @Override
                 public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                     Recipe findRecipe = dataSnapshot.getValue(Recipe.class);
-                    textViewRecipeName.setText(findRecipe.recipeName);
-                    textViewStepName.setText(findRecipe.getStepName(1));
-                    textViewStepIngredientList.setText(findRecipe.getStepIngredients(1));
-                    //imageStepPhoto.setImageBitmap(stepImage);
+                    String numSteps = dataSnapshot.getValue(Recipe.class).numberOfSteps;
+                    int numStepsInt = Integer.valueOf(numSteps);
+                    if(numStepsInt == 1){
 
+                        Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
+                        intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
+                        startActivity(intentRecipeStepComplete);
+
+                    }else {
+                        textViewRecipeName.setText(findRecipe.recipeName);
+                        textViewStepName.setText(findRecipe.getStepName(1));
+                        textViewStepIngredientList.setText(findRecipe.getStepIngredients(1));
+                        //imageStepPhoto.setImageBitmap(stepImage);
+                    }
                 }
                 @Override
                 public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {                }
@@ -126,58 +143,223 @@ public class RecipeStepBase extends AppCompatActivity implements View.OnClickLis
 
             floatingActionButtonNext2.show();
             floatingActionButtonNext.hide();
-            floatingActionButtonNext3.hide();
 
             } else if (v == floatingActionButtonNext2) {
 
-            String findName2 = textViewRecipeName.getText().toString();
+            String findName = textViewRecipeName.getText().toString();
 
-            myRef.orderByChild("recipeName").equalTo(findName2).addChildEventListener(new ChildEventListener() {
+            myRef.orderByChild("recipeName").equalTo(findName).addChildEventListener(new ChildEventListener() {
 
 
                 @Override
                 public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                     Recipe findRecipe = dataSnapshot.getValue(Recipe.class);
-                    textViewRecipeName.setText(findRecipe.recipeName);
-                    textViewStepName.setText(findRecipe.getStepName(2));
-                    textViewStepIngredientList.setText(findRecipe.getStepIngredients(2));
-                    //imageStepPhoto.setImageBitmap(stepImage);
 
+                    String numSteps = dataSnapshot.getValue(Recipe.class).numberOfSteps;
+                    int numStepsInt = Integer.valueOf(numSteps);
+                    if(numStepsInt == 2){
+
+                        Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
+                        intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
+                        startActivity(intentRecipeStepComplete);
+
+                    }else {
+                        textViewRecipeName.setText(findRecipe.recipeName);
+                        textViewStepName.setText(findRecipe.getStepName(2));
+                        textViewStepIngredientList.setText(findRecipe.getStepIngredients(2));
+                        //imageStepPhoto.setImageBitmap(stepImage);
+                    }
                 }
 
                 @Override
-                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                }
-
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {                }
                 @Override
-                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                }
-
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
                 @Override
-                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                }
-
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
                 @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
+                public void onCancelled(@NonNull DatabaseError databaseError) {                }
             });
 
 
             floatingActionButtonNext3.show();
             floatingActionButtonNext2.hide();
-            floatingActionButtonNext.hide();
 
         } else if (v == floatingActionButtonNext3) {
 
-            Intent intentRecipeStepComplete = new Intent(this, RecipeStepComplete.class);
+            String findName = textViewRecipeName.getText().toString();
+
+            myRef.orderByChild("recipeName").equalTo(findName).addChildEventListener(new ChildEventListener() {
+
+
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Recipe findRecipe = dataSnapshot.getValue(Recipe.class);
+
+                    String numSteps = dataSnapshot.getValue(Recipe.class).numberOfSteps;
+                    int numStepsInt = Integer.valueOf(numSteps);
+                    if (numStepsInt == 3) {
+
+                        Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
+                        intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
+                        startActivity(intentRecipeStepComplete);
+
+                    } else {
+                         textViewRecipeName.setText(findRecipe.recipeName);
+                        textViewStepName.setText(findRecipe.getStepName(3));
+                        textViewStepIngredientList.setText(findRecipe.getStepIngredients(3));
+                        //imageStepPhoto.setImageBitmap(stepImage);
+                    }
+
+
+                }
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) { }
+            });
+
+            floatingActionButtonNext4.show();
+            floatingActionButtonNext3.hide();
+
+
+        } else if (v == floatingActionButtonNext4) {
+
+            String findName = textViewRecipeName.getText().toString();
+
+            myRef.orderByChild("recipeName").equalTo(findName).addChildEventListener(new ChildEventListener() {
+
+
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Recipe findRecipe = dataSnapshot.getValue(Recipe.class);
+
+                    String numSteps = dataSnapshot.getValue(Recipe.class).numberOfSteps;
+                    int numStepsInt = Integer.valueOf(numSteps);
+                    if(numStepsInt == 4){
+
+                        Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
+                        intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
+                        startActivity(intentRecipeStepComplete);
+
+                    }else {
+                        textViewRecipeName.setText(findRecipe.recipeName);
+                        textViewStepName.setText(findRecipe.getStepName(4));
+                        textViewStepIngredientList.setText(findRecipe.getStepIngredients(4));
+                        //imageStepPhoto.setImageBitmap(stepImage);
+                    }
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {                }
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {                }
+            });
+
+            floatingActionButtonNext5.show();
+            floatingActionButtonNext4.hide();
+
+        } else if (v == floatingActionButtonNext5) {
+
+            String findName = textViewRecipeName.getText().toString();
+
+            myRef.orderByChild("recipeName").equalTo(findName).addChildEventListener(new ChildEventListener() {
+
+
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Recipe findRecipe = dataSnapshot.getValue(Recipe.class);
+
+                    String numSteps = dataSnapshot.getValue(Recipe.class).numberOfSteps;
+                    int numStepsInt = Integer.valueOf(numSteps);
+                    if(numStepsInt == 5){
+
+                        Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
+                        intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
+                        startActivity(intentRecipeStepComplete);
+
+                    }else {
+                        textViewRecipeName.setText(findRecipe.recipeName);
+                        textViewStepName.setText(findRecipe.getStepName(5));
+                        textViewStepIngredientList.setText(findRecipe.getStepIngredients(5));
+                        //imageStepPhoto.setImageBitmap(stepImage);
+                    }
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {                }
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {                }
+            });
+
+            floatingActionButtonNext6.show();
+            floatingActionButtonNext5.hide();
+
+        } else if (v == floatingActionButtonNext6) {
+
+            String findName = textViewRecipeName.getText().toString();
+
+            ChildEventListener childEventListener = myRef.orderByChild("recipeName").equalTo(findName).addChildEventListener(new ChildEventListener() {
+
+
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Recipe findRecipe = dataSnapshot.getValue(Recipe.class);
+
+                    String numSteps = dataSnapshot.getValue(Recipe.class).numberOfSteps;
+                    int numStepsInt = Integer.valueOf(numSteps);
+
+
+                    if (numStepsInt==6) {
+
+                        Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
+                        intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
+                        startActivity(intentRecipeStepComplete);
+
+                    } else {
+
+                        textViewRecipeName.setText(findRecipe.recipeName);
+                        textViewStepName.setText(findRecipe.getStepName(6));
+                        textViewStepIngredientList.setText(findRecipe.getStepIngredients(6));
+                        //imageStepPhoto.setImageBitmap(stepImage);
+
+                    }
+                }
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) { }
+            });
+
+            floatingActionButtonNext7.show();
+            floatingActionButtonNext6.hide();
+
+        } else if (v == floatingActionButtonNext7) {
+
+
+            Intent intentRecipeStepComplete = new Intent(RecipeStepBase.this, RecipeStepComplete.class);
             intentRecipeStepComplete.putExtra("passedRecipeName", textViewRecipeName.getText().toString());
             startActivity(intentRecipeStepComplete);
         }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
